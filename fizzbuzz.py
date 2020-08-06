fee1773c504d626838d7319625b6242290da95ac
hirokigolf <kato@cancerscan.jp>
2020-08-06 11:45:04 +0900

    buzzを追加

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,6 +4,8 @@ def fizzbuzz():
             print('fizzbuzz')
         elif n%3==0:
             print('fizz')
+        elif n%5==0:
+            print('buzz')
         else:
             print(n)
 fizzbuzz()
